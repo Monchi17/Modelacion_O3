9416f1421089facf791d22329e5e2d6eef21f2e4
ÁLVAREZ ARRIAGADA MONSERRATH YSIS SERENA <167034230+Monchi17@users.noreply.github.com>
2025-01-10 08:17:14 -0300

    Update app_modelacion.py

--- app_modelacion.py
+++ app_modelacion.py
@@ -26,7 +26,7 @@ opcion_seleccionada = st.selectbox(
 largo_casa, ancho_casa = opciones[opcion_seleccionada]
 
 
-st.write(f"Dimensiones ingresadas: Largo = {largo_casa} m, Ancho = {ancho_casa} m")
+st.write(f"Area del plano {largo_casa * ancho_casa} M^2")
 
 # Clase para definir habitaciones
 class Habitacion:
