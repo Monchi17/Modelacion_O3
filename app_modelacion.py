ab357a0ee693811fda3f70af2cb2c906a41db87f
ÁLVAREZ ARRIAGADA MONSERRATH YSIS SERENA <167034230+Monchi17@users.noreply.github.com>
2025-06-13 19:57:50 -0400

    Update app_modelacion.py

--- app_modelacion.py
+++ app_modelacion.py
@@ -332,19 +332,19 @@ def generar_planos_v2_desde_v1_sin_p5(combinaciones_sin_p5, largo_casa, ancho_ca
     todos_los_planos = []
     
     for i in range(len(combinaciones_sin_p5)):
-    casa1 = Casa(largo_casa, ancho_casa)
-    casa1.agregar_habitacion(Habitacion("P7", [(0, 5.850), (2.585, 5.850), (2.585, 9.765), (0, 9.765)]))
-    casa1.agregar_habitacion(Habitacion("P6", [(2.585, 5.850), (4.880, 5.850), (4.880, 8.280), (2.585, 8.280)]))
-    casa1.agregar_habitacion(Habitacion("P8", [(2.585, 8.290), (4.880, 8.290), (4.880, 9.765), (2.585, 9.765)]))
-    casa1.habitaciones.extend(combinaciones_sin_p5[i].habitaciones)
-    todos_los_planos.append(casa1)
+        casa1 = Casa(largo_casa, ancho_casa)
+        casa1.agregar_habitacion(Habitacion("P7", [(0, 5.850), (2.585, 5.850), (2.585, 9.765), (0, 9.765)]))
+        casa1.agregar_habitacion(Habitacion("P6", [(2.585, 5.850), (4.880, 5.850), (4.880, 8.280), (2.585, 8.280)]))
+        casa1.agregar_habitacion(Habitacion("P8", [(2.585, 8.290), (4.880, 8.290), (4.880, 9.765), (2.585, 9.765)]))
+        casa1.habitaciones.extend(combinaciones_sin_p5[i].habitaciones)
+        todos_los_planos.append(casa1)
     
-    casa2 = Casa(largo_casa, ancho_casa)
-    casa2.agregar_habitacion(Habitacion("P6", [(0, 5.839), (2.295, 5.839), (2.295, 8.272), (0, 8.272)]))
-    casa2.agregar_habitacion(Habitacion("P8", [(0, 8.272), (2.295, 8.272), (2.295, 9.759), (0, 9.759)]))
-    casa2.agregar_habitacion(Habitacion("P7", [(2.295, 5.839), (4.880, 5.839), (4.880, 9.759), (2.295, 9.759)]))
-    casa2.habitaciones.extend(combinaciones_sin_p5[i].habitaciones)
-    todos_los_planos.append(casa2)
+        casa2 = Casa(largo_casa, ancho_casa)
+        casa2.agregar_habitacion(Habitacion("P6", [(0, 5.839), (2.295, 5.839), (2.295, 8.272), (0, 8.272)]))
+        casa2.agregar_habitacion(Habitacion("P8", [(0, 8.272), (2.295, 8.272), (2.295, 9.759), (0, 9.759)]))
+        casa2.agregar_habitacion(Habitacion("P7", [(2.295, 5.839), (4.880, 5.839), (4.880, 9.759), (2.295, 9.759)]))
+        casa2.habitaciones.extend(combinaciones_sin_p5[i].habitaciones)
+        todos_los_planos.append(casa2)
 
     # for casa_base in combinaciones_sin_p5:
     #     casa1 = Casa(largo_casa, ancho_casa, tipo="Tipo 1")
